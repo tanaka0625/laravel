1e899d0713e5cc2e0ffaeac3ad93d55cc554abdc
tanaka0625 <tanasou6@gmail.com>
2022-12-09 12:02:49 +0900

    4-26 バリデータ　エラーメッセージのカスタマイズ

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -26,11 +26,24 @@ class HelloController extends Controller
     public function post(Request $request)
     {
 
-        $validator = Validator::make($request->all(), [
+        $rules = [
             'name' => 'required',
             'mail' => 'email',
             'age' => 'numeric|between:0,150'
-        ]);
+        ];
+
+        $message = [
+            'name.required' => '名前は必ず入力して下さい。',
+            'mail.email' => 'メールアドレスが必要です。',
+            'age.numeric' => '年齢を整数で記入下さい。',
+            'age.between' => '年齢は0~150の間で入力下さい。'
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $message);
+        if($validator->fails()){
+            return redirect('/hello')->withErrors($validator)->withInput();
+        }
+
         if($validator->fails()){
             return redirect('/hello')->withErrors($validator)->withInput();
         }
