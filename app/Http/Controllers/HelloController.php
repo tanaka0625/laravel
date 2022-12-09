3975382d0e96e0f2db302c2c75f10b0143c3c5ca
tanaka0625 <tanasou6@gmail.com>
2022-12-09 11:52:04 +0900

    4-25 バリデータ　クエリー文字列

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,23 +5,35 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
     public function index(Request $request) {
+        $validator = Validator::make($request->query(),[
+            'id' => 'required',
+            'pass' => 'required'
+        ]);
+        if($validator->fails()){
+            $msg = 'クエリ―に問題があります。';
+        }else{
+            $msg = 'ID/PASSを受付ました。フォームを入力ください。';
+        }
 
-        return view('hello.index', ['msg'=>'フォームを入力:']);
+        return view('hello.index', ['msg'=>$msg,]);
     }
 
-    public function post(HelloRequest $request)
+    public function post(Request $request)
     {
-        $validate_rule = [
+
+        $validator = Validator::make($request->all(), [
             'name' => 'required',
             'mail' => 'email',
-            'age' => 'numeric|between:0,150',
-        ];
-        $this->validate($request, $validate_rule);
+            'age' => 'numeric|between:0,150'
+        ]);
+        if($validator->fails()){
+            return redirect('/hello')->withErrors($validator)->withInput();
+        }
         return view('hello.index', ['msg'=>'正しく入力されました!']);
-
     }
 }
