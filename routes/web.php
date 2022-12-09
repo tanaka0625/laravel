acd1f94f88f43bb59a001ef8f9cba874bd1e23c9
tanaka0625 <tanasou6@gmail.com>
2022-12-09 16:33:46 +0900

    5-14~16 delete文

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,8 @@ Route::post('hello/add', [HelloController::class, 'create']);
 Route::get('hello/edit', [HelloController::class, 'edit']);
 Route::post('hello/edit', [HelloController::class, 'update']);
 
+Route::get('hello/del', [HelloController::class, 'del']);
+Route::post('hello/del', [HelloController::class, 'remove']);
+
 
 // Route::get('/', [TopController::class, 'index']);
