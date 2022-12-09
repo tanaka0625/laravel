201aef9ba2bcd50137e9f7d01417df0a091f5d8b
tanaka0625 <tanasou6@gmail.com>
2022-12-09 13:38:41 +0900

    4-32 Validator

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,43 +10,13 @@ use Validator;
 class HelloController extends Controller
 {
     public function index(Request $request) {
-        $validator = Validator::make($request->query(),[
-            'id' => 'required',
-            'pass' => 'required'
-        ]);
-        if($validator->fails()){
-            $msg = 'クエリ―に問題があります。';
-        }else{
-            $msg = 'ID/PASSを受付ました。フォームを入力ください。';
-        }
 
-        return view('hello.index', ['msg'=>$msg,]);
+        return view('hello.index', ['msg'=>'フォームを入力ください']);
     }
 
-    public function post(Request $request)
+    public function post(HelloRequest $request)
     {
 
-        $rules = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric|between:0,150'
-        ];
-
-        $message = [
-            'name.required' => '名前は必ず入力して下さい。',
-            'mail.email' => 'メールアドレスが必要です。',
-            'age.numeric' => '年齢を整数で記入下さい。',
-            'age.between' => '年齢は0~150の間で入力下さい。'
-        ];
-
-        $validator = Validator::make($request->all(), $rules, $message);
-        if($validator->fails()){
-            return redirect('/hello')->withErrors($validator)->withInput();
-        }
-
-        if($validator->fails()){
-            return redirect('/hello')->withErrors($validator)->withInput();
-        }
         return view('hello.index', ['msg'=>'正しく入力されました!']);
     }
 }
--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Validator;
+use App\Http\Validators\HelloValidator;
 
 class HelloServiceProvider extends ServiceProvider
 {
@@ -24,8 +26,13 @@ class HelloServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::composer(
-            'hello.index', 'App\Http\Composers\HelloComposer'
-        );
+        // $validator = $this->app['validator'];
+        // $validator->resolver(function($translator, $data, $rules, $message){
+        //     return new HelloValidator($translator, $data, $rules, $message);
+        // });
+
+        Validator::extend('hello', function($attribute, $value, $parameters, $validator){
+            return $value % 2 == 0;
+        });
     }
 }
