5da8d0cb279980fc8bfbae5bc91e07ea6f9ef11e
tanaka0625 <tanasou6@gmail.com>
2022-12-12 10:31:39 +0900

    5-23 orwhere

--- resources/views/hello/show.blade.php
+++ resources/views/hello/show.blade.php
@@ -8,12 +8,16 @@
 @endsection
 
 @section('content')
-    <table>
-        <tr><th>id: </th><td>{{$item->id}}</td></tr>
-        <tr><th>name: </th><td>{{$item->name}}</td></tr>
-        <tr><th>mail: </th><td>{{$item->mail}}</td></tr>
-        <tr><th>age: </th><td>{{$item->age}}</td></tr>
-    </table>
+    @if($items != NULL)
+    @foreach ($items as $item)
+        <table>
+            <tr>
+                <th>id: </th><td>{{$item->id}}</td>
+                <th>name: </th><td>{{$item->name}}</td>
+            </tr>
+        </table>
+    @endforeach
+    @endif
     
 @endsection
 
