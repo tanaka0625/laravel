2db16fb6dfda031937c10931ef9d1686253e4b90
tanaka0625 <tanasou6@gmail.com>
2022-12-13 09:46:27 +0900

    chapter7 RESTful

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\PersonController;
 use App\Http\Controllers\BoardController;
+use App\Http\Controllers\RestappController;
 use App\Http\Middleware\HelloMiddleware;
 
 /*
@@ -53,6 +54,10 @@ Route::get('board', [BoardController::class, 'index']);
 
 Route::get('board/add', [BoardController::class, 'add']);
 Route::post('board/add', [BoardController::class, 'create']);
+
+Route::resource('rest', RestappController::class);
+
+Route::get('hello/rest', [HelloController::class, 'rest']);
 
 
 
