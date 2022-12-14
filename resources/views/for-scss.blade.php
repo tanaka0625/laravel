8b3e9c03b29e59428c96fe1194604b7afe8fb114
tanaka0625 <tanasou6@gmail.com>
2022-12-14 11:54:07 +0900

    primaryを赤色に　 secondaryをhogeに

--- resources/views/for-scss.blade.php
+++ resources/views/for-scss.blade.php
@@ -13,5 +13,23 @@
   <div class="box">
     こんにちわ
   </div>
+
+  <div class="sample-box">
+    <p>おはよう</p>
+  </div>
+
+  <div class="row">
+    <div class="col-md">
+      <button type="button" class="btn btn-primary">Primary</button>
+      <button type="button" class="btn btn-hoge">Secondary</button>
+      <button type="button" class="btn btn-success">Success</button>
+      <button type="button" class="btn btn-danger">Danger</button>
+      <button type="button" class="btn btn-warning">Warning</button>
+      <button type="button" class="btn btn-info">Info</button>
+      <button type="button" class="btn btn-light">Light</button>
+      <button type="button" class="btn btn-dark">Dark</button>
+      <button type="button" class="btn btn-link">Link</button>
+    </div>
+  </div>
 </body>
 </html>
